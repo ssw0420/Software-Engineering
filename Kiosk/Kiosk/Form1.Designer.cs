bc26aba86cc4b879e1b6d3e24a3d740200e8ad91
python181818 <134188237+python181818@users.noreply.github.com>
2024-05-30 23:01:26 +0900

    ProductDetail
    24.05.30
    주문내역 화면 간단 구성

--- Kiosk/Kiosk/Form1.Designer.cs
+++ Kiosk/Kiosk/Form1.Designer.cs
@@ -28,49 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 1024);
+            this.panel1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(1118, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 37);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "별점 등록";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(930, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(208, 37);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "가격";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(707, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 37);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "주문 수량";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(400, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 37);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "상품 이름";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(47, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 37);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "주문시간";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(742, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 54);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "주문내역";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 937);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1336, 87);
+            this.panel2.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.Location = new System.Drawing.Point(65, 962);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(177, 34);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "총 주문 수량";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::Kiosk.Properties.Resources.backtomain;
-            this.pictureBox1.Location = new System.Drawing.Point(294, 132);
+            this.pictureBox1.Image = global::Kiosk.Properties.Resources.orderdetailexit;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 854);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(191, 91);
-            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.Size = new System.Drawing.Size(239, 61);
+            this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Kiosk.Properties.Resources.baguniadd;
-            this.pictureBox2.Location = new System.Drawing.Point(531, 139);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(185, 83);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox2);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1318, 977);
+            this.ControlBox = false;
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Location = new System.Drawing.Point(50, 56);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Kiosk/Kiosk/Form1.cs
+++ Kiosk/Kiosk/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,19 @@ namespace Kiosk
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = Color.FromArgb(235, 224, 220);
+
+            label2.BackColor = Color.Transparent;
+            label2.Parent = panel1;
+        
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form2 newform2=new Form2();
+            newform2.ShowDialog();
         }
     }
 }
