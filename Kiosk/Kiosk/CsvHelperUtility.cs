e7e6ef84d36148432f2f03ad6a02297bc74a293a
ssw0420 <tmddn5645@gmail.com>
2024-06-03 12:28:29 +0900

    Reapply "주문내역"
    This reverts commit ebe4c4b4d87a515a7c72e5ff2fa2ce8b570be6b7.

---
+++ Kiosk/Kiosk/CsvHelperUtility.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk
+{
+    public class CsvHelperUtility
+    {
+        public static List<T> ReadCsv<T>(string filePath, ClassMap<T> map)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap(map);
+                return new List<T>(csv.GetRecords<T>());
+            }
+        }
+    }
+}
