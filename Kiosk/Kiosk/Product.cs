d53f6f1565f1d72693d734473d84b654f7beb6f6
ssw0420 <tmddn5645@gmail.com>
2024-06-03 12:32:38 +0900

    Reapply "주문내역"
    This reverts commit 12b5bdc64763a56c208cedafff114bebe4e88bef.

---
+++ Kiosk/Kiosk/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPath { get; set; }
+        public string ProductCategory { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductContent { get; set; }
+        public int ProductRating { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
