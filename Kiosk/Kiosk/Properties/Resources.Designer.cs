78aaa595b037db157736c20a2d13017e5730a99c
ssw0420 <tmddn5645@gmail.com>
2024-06-03 12:32:44 +0900

    Reapply "."
    This reverts commit b7e9805a33d0ea5c343c9de344759142f7fc6231.

--- Kiosk/Kiosk/Properties/Resources.Designer.cs
+++ Kiosk/Kiosk/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace Kiosk.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap callmanager1 {
+            get {
+                object obj = ResourceManager.GetObject("callmanager1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap chadoljjigae {
             get {
                 object obj = ResourceManager.GetObject("chadoljjigae", resourceCulture);
@@ -333,9 +343,39 @@ namespace Kiosk.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap orderdetailbutton1 {
+            get {
+                object obj = ResourceManager.GetObject("orderdetailbutton1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap orderdetailbutton2 {
+            get {
+                object obj = ResourceManager.GetObject("orderdetailbutton2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap orderdetailexit {
             get {
                 object obj = ResourceManager.GetObject("orderdetailexit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap orderdetailexit1 {
+            get {
+                object obj = ResourceManager.GetObject("orderdetailexit1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
