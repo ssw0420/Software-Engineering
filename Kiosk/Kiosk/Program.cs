42f8d101c1131e4f0759a4b214d7f75c203de944
ssw0420 <tmddn5645@gmail.com>
2024-06-03 12:32:57 +0900

    Revert "Reapply "주문내역""
    This reverts commit e7e6ef84d36148432f2f03ad6a02297bc74a293a.

--- Kiosk/Kiosk/Program.cs
+++ Kiosk/Kiosk/Program.cs
@@ -16,7 +16,7 @@ namespace Kiosk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerMainForm());
+            Application.Run(new OrderDetailPopup());
         }
     }
 }
