91f173a8336d6ee131cdc40f82a72200282a214f
ssw0420 <tmddn5645@gmail.com>
2024-06-03 12:20:30 +0900

    Revert "."
    This reverts commit a562ccae93cd723da2055995e3cb6d7852cbaf1d.

---
+++ Kiosk/Kiosk/Category.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Map(m => m.CategoryId).Name("categoryId");
+            Map(m => m.CategoryName).Name("categoryName");
+        }
+    }
+}
